2661770157e5cd69afb77c8138e76752e27c8616
GracielaMoreno <chelinesmoreno@hotmail.com>
2018-04-18 15:16:59 -0500

    creando variables

---
+++ 01-variables.js
@@ -0,0 +1,25 @@
+console.log('holi');
+var nombre = 'Adrian';
+var edad = 20;
+var deudas = 223.36;
+var FechaNacimiento = new Date();
+var casado = false;
+var estoyVacio = null;
+var noEstoyDefinido = undefined;
+//null undefined 0 -1 1
+//
+if(1) {
+    console.log('Verdadero');
+} else{
+        console.log('falso');
+    }
+
+    //json para propiedades de un objeto, empieza con unas llaves, como buena practica se usaran las comillas simples,
+//no hace falta formatear el codigo.. ctrl a y luego ctralt l para formatear
+var usuario = {
+    "nombre": "Brix",
+    apellido: 'Jarrin',
+    edad: 28,
+};
+
+console.log(usuario)
